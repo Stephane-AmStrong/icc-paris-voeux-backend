dd37adcb3322fc2ffd9fa6d4a7e39b798dd515a0
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-12-29 00:01:03 +0100

    prepare to deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://localhost:3000",
-  "http://localhost:5173",
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com",
-  "https://ICC Paris Centre.onrender.com",
-];
+const allowedOrigins = ["https://ICC Paris Centre.onrender.com"];
 
 module.exports = allowedOrigins;
