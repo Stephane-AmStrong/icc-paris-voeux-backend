6ad4eb1d6b785b50746296a93aaf177d98dcf45d
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-19 16:08:58 +0200

    adds extension

---
+++ Presentation/WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json.Serialization;
+using Application.Services.Abstractions;
+using Domain.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using Persistence.Repository;
+using Services;
+using WebApi.Middleware;
+
+namespace WebApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.WithOrigins(
+                    "http://localhost:4200",
+                    "https://localhost:4200",
+                    "http://localhost:5173",
+                    "https://localhost:5173"
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+
+        });
+    }
+
+    public static void ConfigureMongoDB(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MongoDB");
+        var databaseName = configuration.GetConnectionString("DatabaseName");
+
+        services.AddSingleton<IMongoClient>(serviceProvider =>
+        {
+            return new MongoClient(connectionString);
+        });
+
+        services.AddScoped(serviceProvider =>
+        {
+            var client = serviceProvider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase(databaseName);
+        });
+    }
+
+    public static void ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IWishesRepository, WishsesRepository>();
+    }
+
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IWishesService, WishesService>();
+    }
+
+
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+                        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Icc paris centre Web API", Version = "v1" }));
+    }
+
+    public static void ConfigureJsonOptions(this IServiceCollection services)
+    {
+        services.Configure<JsonOptions>(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+    }
+
+    public static void ConfigureGlobalExceptionHandling(this IServiceCollection services)
+    {
+        services.AddScoped<EndpointLoggingMiddleware>();
+        services.AddScoped<ExceptionHandlingMiddleware>();
+    }
+
+    public static RouteHandlerBuilder WithRequestValidation<TRequest>(this RouteHandlerBuilder builder)
+    {
+        return builder.AddEndpointFilter<Filters.ValidationFilter<TRequest>>().ProducesValidationProblem();
+    }
+}
