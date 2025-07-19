6c216aa6fd9240e9c18c5df0cf2ab4d9ad4d1ec8
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-19 16:08:25 +0200

    add repositories

---
+++ Infrastructure/Persistence/Repository/RepositoryBase.cs
@@ -0,0 +1,88 @@
+using System.Linq.Expressions;
+using Domain.Repositories.Abstractions;
+using Domain.Shared.Common;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Persistence.Repository;
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+{
+    protected IMongoCollection<T> Collection { get; }
+
+    protected RepositoryBase(IMongoDatabase database, string collectionName) => Collection = database.GetCollection<T>(collectionName);
+
+
+    public Task<List<T>> BaseFindByConditionAsync(QueryParameters<T> parameters, CancellationToken cancellationToken)
+    {
+        List<FilterDefinition<T>> filters = [];
+
+        // Add expression filter if any
+        if (parameters.Expression != null) filters.Add(Builders<T>.Filter.Where(parameters.Expression));
+
+        // Add text search filter if any
+        if (!string.IsNullOrWhiteSpace(parameters.SearchTerm))
+        {
+            var stringProps = typeof(T).GetProperties()
+                .Where(p => p.PropertyType == typeof(string));
+
+            var regexFilters = stringProps
+                .Select(p => Builders<T>.Filter.Regex(p.Name, new BsonRegularExpression(parameters.SearchTerm, "i")));
+
+            filters.AddRange(regexFilters);
+        }
+
+        // Combine all filters with AND if more than one, otherwise Empty
+        var filter = filters.Count switch
+        {
+            0 => Builders<T>.Filter.Empty,
+            1 => filters[0],
+            _ => Builders<T>.Filter.And(filters)
+        };
+
+        var find = Collection.Find(filter);
+
+        if (!string.IsNullOrWhiteSpace(parameters.OrderBy)) find = find.Sort(Builders<T>.Sort.Ascending(parameters.OrderBy));
+
+        find = find.Skip((parameters.Page - 1) * parameters.PageSize).Limit(parameters.PageSize);
+
+        return find.ToListAsync(cancellationToken);
+    }
+
+    public Task<List<T>> BaseFindByConditionAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken)
+    {
+        return Collection.Find(expression).ToListAsync(cancellationToken);
+    }
+
+    public Task BaseCreateAsync(T entity, CancellationToken cancellationToken)
+    {
+        return Collection.InsertOneAsync(entity, cancellationToken: cancellationToken);
+    }
+
+    public Task BaseUpdateAsync(T entity, CancellationToken cancellationToken)
+    {
+        var idProperty = typeof(T).GetProperty("Id") ?? throw new InvalidOperationException("The entity must have a property 'Id'.");
+        var idValue = idProperty.GetValue(entity);
+        var filter = Builders<T>.Filter.Eq("Id", idValue);
+
+        return Collection.ReplaceOneAsync(filter, entity, cancellationToken: cancellationToken);
+    }
+
+    public Task BaseDeleteAsync(T entity, CancellationToken cancellationToken)
+    {
+        var idProperty = typeof(T).GetProperty("Id") ?? throw new InvalidOperationException("The entity must have a property 'Id'.");
+        var idValue = idProperty.GetValue(entity);
+        var filter = Builders<T>.Filter.Eq("Id", idValue);
+
+        return Collection.DeleteOneAsync(filter, cancellationToken);
+    }
+
+    private static string GetPropertyName<TKey>(Expression<Func<T, TKey>> expression)
+    {
+        if (expression.Body is MemberExpression member)
+            return member.Member.Name;
+        if (expression.Body is UnaryExpression unary && unary.Operand is MemberExpression memberOperand)
+            return memberOperand.Member.Name;
+        throw new ArgumentException("Invalid orderBy expression");
+    }
+}
---
+++ Infrastructure/Persistence/Repository/WishsesRepository.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+using System.Linq.Expressions;
+
+using Domain.Entities;
+using Domain.Repositories.Abstractions;
+using Domain.Shared.Common;
+using MongoDB.Driver;
+
+namespace Persistence.Repository;
+
+public sealed class WishsesRepository(IMongoDatabase database) : RepositoryBase<Wish>(database, "WT_Clients"), IWishesRepository
+{
+    public async Task CreateAsync(Wish wish, CancellationToken cancellationToken)
+    {
+        await BaseCreateAsync(wish, cancellationToken);
+    }
+
+    public Task DeleteAsync(Wish wish, CancellationToken cancellationToken)
+    {
+        return BaseDeleteAsync(wish, cancellationToken);
+    }
+
+    public Task<List<Wish>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return BaseFindByConditionAsync(client => true, cancellationToken);
+    }
+
+    public async Task<Wish?> GetByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        var clients = await BaseFindByConditionAsync(client => client.Id == id, cancellationToken);
+        return clients.FirstOrDefault();
+    }
+
+    public Task<List<Wish>> GetByConditionAsync(QueryParameters<Wish> queryParameters, CancellationToken cancellationToken)
+    {
+        return BaseFindByConditionAsync(queryParameters, cancellationToken);
+    }
+
+    public Task UpdateAsync(Wish wish, CancellationToken cancellationToken)
+    {
+        return BaseUpdateAsync(wish, cancellationToken);
+    }
+}
