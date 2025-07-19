a6943c09dedf539415532722da4aa1600a3cbfe0
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-19 16:07:36 +0200

    adds shared common

---
+++ Core/Domain/Shared.Common/QueryParameters.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Shared.Common;
+
+public abstract record QueryParameters<T>
+{
+    public Expression<Func<T, bool>>? Expression { get; set; }
+    public int Page { get; set; } = 1;
+    public string? OrderBy { get; set; }
+    public string? SearchTerm { get; set; }
+
+    private const int MaxPageSize = 50;
+    private int _pageSize = 10;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = Math.Min(value, MaxPageSize);
+    }
+}
