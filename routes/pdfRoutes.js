b59812528ae338e6e0488fd750411851ac7e882a
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-19 10:20:27 +0200

    removes js server

--- routes/pdfRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const wishesController = require("../controllers/pdfController");
-
-router.route("/print").get(wishesController.getPDF);
-
-module.exports = router;
