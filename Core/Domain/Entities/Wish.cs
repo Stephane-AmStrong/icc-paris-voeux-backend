384443720006f8d785a199f6505ea3ad01d2f274
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-19 16:07:07 +0200

    adds entities

---
+++ Core/Domain/Entities/Wish.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+public class Wish
+{
+    public string Id { get; init; }
+    public string Spiritually { get; init; }
+    public string FamiliallyRelationally { get; init; }
+    public string FinanciallyMaterially { get; init; }
+    public string ProfessionallyAcademically { get; init; }
+    public string Other { get; init; }
+    public string Email { get; init; }
+};
+
