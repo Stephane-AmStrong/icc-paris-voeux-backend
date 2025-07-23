bbbabf460ceda6e54caecfe1c6523a72f4cca860
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-23 13:43:34 +0200

    Adds Base Query Parameters

--- Core/Domain/Entities/Wish.cs
+++ Core/Domain/Entities/Wish.cs
@@ -2,14 +2,12 @@
 
 namespace Domain.Entities;
 
-public class Wish
+public class Wish : BaseEntity
 {
-    public string Id { get; init; }
     public string Spiritually { get; init; }
     public string FamiliallyRelationally { get; init; }
     public string FinanciallyMaterially { get; init; }
     public string ProfessionallyAcademically { get; init; }
     public string Other { get; init; }
     public string Email { get; init; }
-};
-
+}
