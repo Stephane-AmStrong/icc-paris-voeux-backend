122e45567c1aaf0a9037be71742f2f58ed3c3ab8
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-23 13:46:02 +0200

    Adds pagination and filtering

--- Core/Domain/Repositories.Abstractions/IWishesRepository.cs
+++ Core/Domain/Repositories.Abstractions/IWishesRepository.cs
@@ -8,7 +8,8 @@ namespace Domain.Repositories.Abstractions;
 
 public interface IWishesRepository
 {
-    Task<PagedList<Wish>> GetPagedListByQueryAsync(QueryParameters<Wish> queryParameters, CancellationToken cancellationToken);
+    Task<PagedList<Wish>> GetPagedListByQueryAsync(BaseQueryParameters<Wish> baseQueryParameters, CancellationToken cancellationToken);
+    Task<List<Wish>> FindByConditionAsync(Expression<Func<Wish, bool>> expression, CancellationToken cancellationToken);
     Task<Wish?> GetByIdAsync(string id, CancellationToken cancellationToken);
     Task CreateAsync(Wish wish, CancellationToken cancellationToken);
     Task UpdateAsync(Wish wish, CancellationToken cancellationToken);
--- Infrastructure/Persistence/Repository/WishesRepository.cs
+++ Infrastructure/Persistence/Repository/WishesRepository.cs
@@ -10,6 +10,22 @@ namespace Persistence.Repository;
 
 public sealed class WishesRepository(IMongoDatabase database) : RepositoryBase<Wish>(database, "Wishes"), IWishesRepository
 {
+    public Task<PagedList<Wish>> GetPagedListByQueryAsync(BaseQueryParameters<Wish> baseQueryParameters, CancellationToken cancellationToken)
+    {
+        return BaseQueryWithFiltersAsync(baseQueryParameters, cancellationToken);
+    }
+
+    public Task<List<Wish>> FindByConditionAsync(Expression<Func<Wish, bool>> expression, CancellationToken cancellationToken)
+    {
+        return BaseFindByConditionAsync(expression, cancellationToken);
+    }
+
+    public async Task<Wish?> GetByIdAsync(string id, CancellationToken cancellationToken)
+    {
+        var wishes = await BaseFindByConditionAsync(wish => wish.Id == id, cancellationToken);
+        return wishes.FirstOrDefault();
+    }
+
     public async Task CreateAsync(Wish wish, CancellationToken cancellationToken)
     {
         await BaseCreateAsync(wish, cancellationToken);
@@ -18,22 +34,6 @@ public sealed class WishesRepository(IMongoDatabase database) : RepositoryBase<W
     public Task DeleteAsync(Wish wish, CancellationToken cancellationToken)
     {
         return BaseDeleteAsync(wish, cancellationToken);
-    }
-
-    public Task<List<Wish>> GetAllAsync(CancellationToken cancellationToken)
-    {
-        return BaseFindByConditionAsync(client => true, cancellationToken);
-    }
-
-    public async Task<Wish?> GetByIdAsync(string id, CancellationToken cancellationToken)
-    {
-        var clients = await BaseFindByConditionAsync(client => client.Id == id, cancellationToken);
-        return clients.FirstOrDefault();
-    }
-
-    public Task<PagedList<Wish>> GetPagedListByQueryAsync(QueryParameters<Wish> queryParameters, CancellationToken cancellationToken)
-    {
-        return BaseQueryWithFiltersAsync(queryParameters, cancellationToken);
     }
 
     public Task UpdateAsync(Wish wish, CancellationToken cancellationToken)
