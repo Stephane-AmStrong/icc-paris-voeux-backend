05cd87a3398144202fac28021cc2a45b7bfb1655
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-25 21:50:51 +0200

    Adds handlers

---
+++ Core/Application/Abstractions/Handlers/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Abstractions.Handlers;
+
+public interface ICommand;
+public interface ICommand<TResult> : ICommand;
+
+public interface ICommandHandler<in TCommand> where TCommand : ICommand
+{
+    Task HandleAsync(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand, TResult> where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken);
+}
