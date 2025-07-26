8ca2abaffc3e1d2903a01af2b3129e4d507e56c8
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-07-26 13:55:05 +0200

    refactor

--- Core/Application/Abstractions/Handlers/IQueryHandler.cs
+++ Core/Application/Abstractions/Handlers/IQueryHandler.cs
@@ -4,5 +4,5 @@ public interface IQuery<TResult>;
 
 public interface IQueryHandler<in TQuery, TResult> where TQuery : IQuery<TResult>
 {
-    ValueTask<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
+    Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
 }
